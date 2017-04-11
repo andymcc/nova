892aac64398d62744ab2931743703956e0b0cb20
Andy McCrae <andy.mccrae@gmail.com>
2017-04-11 14:19:44 +0100

    Allow CONTENT_LENGTH to be present but empty
    The CONTENT_LENGTH environ can be present, but empty, which returns
    None, and causes a ValueError when attempting to use .int().
    
    This patch removes the setting of CONTENT_LENGTH to an integer, but
    instead ensures that if CONTENT_LENGTH is not empty it is an integer, to
    prevent a situation where a bogus "CONTENT_LENGTH" header is specified.
    
    Additionally, as the CONTENT_TYPE environ can similarly be present but
    empty, we should .get() it in a similar fashion to ensure it isn't
    present but None when CONTENT_LENGTH is specified.
    
    Change-Id: I66b6f9afbea8bf037997a59ba0b976f83c9825fb
    Closes-Bug: #1681843

--- nova/api/openstack/placement/handler.py
+++ nova/api/openstack/placement/handler.py
@@ -177,13 +177,21 @@ class PlacementHandler(object):
                 raise webob.exc.HTTPForbidden(
                     _('admin required'),
                     json_formatter=util.json_error_formatter)
-        # Check that an incoming request with a content-length
-        # header also has a content-type header. If not raise a 400.
-        if int(environ.get('CONTENT_LENGTH', 0)):
-            if 'CONTENT_TYPE' not in environ:
-                raise webob.exc.HTTPBadRequest(
-                    _('content-type header required'),
-                    json_formatter=util.json_error_formatter)
+        # Check that an incoming request with a content-length header
+        # that is an integer > 0 and not empty, also has a content-type
+        # header that is not empty. If not raise a 400.
+        try:
+            content_length = environ.get('CONTENT_LENGTH')
+            if (content_length and (int(content_length) > 0) and
+               not environ.get('CONTENT_TYPE')):
+                    raise webob.exc.HTTPBadRequest(
+                        _('content-type header required when '
+                          'content-length > 0'),
+                        json_formatter=util.json_error_formatter)
+        except ValueError as exc:
+            raise webob.exc.HTTPBadRequest(
+                _('content-length header must be an integer'),
+                json_formatter=util.json_error_formatter)
         try:
             return dispatch(environ, start_response, self._map)
         # Trap the NotFound exceptions raised by the objects used
